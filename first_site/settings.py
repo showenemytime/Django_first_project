61d25b2d63318bc3004f27ec0dd079cd07face1a
showenemytime <az.showtime@yandex.ru>
2023-05-23 12:11:59 +0700

    day4. l19. Добавил base шаблон. использовал {% block %}, include для navbar, extends для index и category

--- first_site/settings.py
+++ first_site/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'first_site.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'tamplates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
